0176a3af16162dd43dc8a58e96f6bc2a0e0d4422
CeeDeeBee <cdbarnes98@gmail.com>
2020-08-07 12:48:47 -0400

    Add ex4 solution

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,18 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # build hash table
+    positives = a.copy()
+    table = {}
+    for num in a:
+        if num < 0:
+            positives.remove(num)
+            table[num] = True
+
+    result = []
+    for num in positives:
+        if table.get(num * -1):
+            result.append(num)
 
     return result
 
